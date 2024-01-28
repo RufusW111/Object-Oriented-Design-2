0ec5e4ad53aa1efd520e827d2e653a99304693c2
Rufus White <WhiteR@StPaulsSchool.org.uk>
2024-01-28 14:28:15 +0000

    Add project files.

---
+++ Object Oriented Design/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Object_Oriented_Design
+{
+    public class Customer
+    {
+        //Properties
+        public string UserName {  get; set; }
+        public string Password { get; private set; }
+        public string Email {  get; set; }
+        public int ID { get; set; }
+
+        //Constructor
+        public Customer (string username, string password, string email)
+        {
+            UserName = username;
+            Password = password;
+            Email = email;
+            Random random = new();
+            ID = random.Next(1, 1000);
+        }
+        //Method
+        public void Email_Change(string newEmail)
+        {
+            Email = newEmail;
+        }
+
+        
+
+    }
+}
